4e9cb8b449ba47ff3345520b5205bf0b8fb1fa8f
Marcelo Bettini <bettinimarcelo@gmail.com>
2021-03-22 21:37:46 -0300

    cards in characters list

--- src/components/CharacterCard.vue
+++ src/components/CharacterCard.vue
@@ -1,32 +1,39 @@
 <template>
-<div>
-  <b-card no-body class="overflow-hidden" style="max-width: 540px;">
-    <b-row no-gutters>
-      <b-col md="6">
-        <b-card-img src="https://picsum.photos/400/400/?image=20" alt="Image" class="rounded-0"></b-card-img>
-      </b-col>
-      <b-col md="6">
-        <b-card-body title="Horizontal Card">
-          <b-card-text>
-            {{character.name}}
-          </b-card-text>
-        </b-card-body>
-      </b-col>
-    </b-row>
-  </b-card>
-</div>
+  <div>
+    <b-card no-body class="overflow-hidden" style="max-width: 540px;">
+      <b-row no-gutters>
+        <b-col md="6">
+          <b-card-img
+            :src="character.image"
+            alt="Image"
+            class="rounded-0"
+          ></b-card-img>
+        </b-col>
+        <b-col md="6">
+          <b-card-body title="Horizontal Card">
+            <b-card-text>
+              {{character.name}}
+            </b-card-text>
+          </b-card-body>
+        </b-col>
+      </b-row>
+    </b-card>
+  </div>
 </template>
 
 <script>
-import { mapState } from 'vuex';
-    export default {
-        computed: {
-            ...mapState(['characters'])
-        }
-        
-    }
+import { mapState } from "vuex";
+export default {
+  props: {
+    character: {
+      type: Object,
+      required: true,
+    },
+  },
+  computed: {
+    ...mapState(["characters"]),
+  },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/views/Characters.vue
+++ src/views/Characters.vue
@@ -1,12 +1,14 @@
 <template>
-  <div class="container">    
-    {{characters}}
+  <div class="container">  
+    <CharacterCard v-for="character in characters" :key="character.id" :character="character"></CharacterCard>    
     
   </div>
 </template>
 <script>
+import CharacterCard from '../components/CharacterCard'
 import {mapState, mapActions} from 'vuex'
 export default { 
+  components: { CharacterCard},
   computed:{
     ...mapState(['characters', 'pagination', 'page'])
   },
