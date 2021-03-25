86747cafeb4306a9148ff50c4ca74ab243414a9c
Marcelo Bettini <bettinimarcelo@gmail.com>
2021-03-25 20:14:53 -0300

    episodes pagination done!

--- src/store/index.js
+++ src/store/index.js
@@ -1,26 +1,32 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {    
+  state: {
     data: [],
     pagination: [],
     page: 1,
-    baseURL: 'https://rickandmortyapi.com/api/'
-  },  
-  mutations: {    
-    loadCharacters(state, rawData) {      
+    baseURL: "https://rickandmortyapi.com/api/",
+  },
+  mutations: {
+    loadCharacters(state, rawData) {
       state.data = rawData.results;
       state.pagination = rawData.info;
     },
+    pageUp(state) {
+      state.page += 1;
+    },
+    pageDown(state) {
+      state.page -= 1;
+    },
   },
   actions: {
-    getData: async function({ commit }, payload) {      
+    getData: async function({ commit }, payload) {
       const data = await fetch(`${this.state.baseURL}${payload}`);
-      const rawData = await data.json();      
-      commit('loadCharacters', rawData )
+      const rawData = await data.json();
+      commit("loadCharacters", rawData);
     },
   },
   modules: {},
--- src/views/Locations.vue
+++ src/views/Locations.vue
@@ -20,17 +20,74 @@
           </tr>
         </tbody>
       </table>
-    </div>    
+    </div>
+    <div class="container d-flex flex-wrap justify-content-center ">
+      <nav aria-label="Page navigation example">
+        <ul class="pagination ">
+          <li class="page-item">
+            <button
+              type="button"
+              class="btn btn-primary rounded"
+              :disabled="prevDisabled"
+              aria-label="Previous"
+              @click="prevPage"
+            >
+              <span aria-hidden="true">&laquo; prev</span>
+              <span class="sr-only">Previous</span>
+            </button>
+          </li>
+          <li class="actualPage align-self-center p-2 mx-2 btn-primary rounded">
+            {{ `${this.page} / ${pagination.pages}` }}
+          </li>
+          <li class="page-item">
+            <button
+              type="button"
+              class="btn btn-primary rounded"
+              :disabled="nextDisabled"
+              aria-label="Next"
+              @click="nextPage"
+            >
+              <span aria-hidden="true">next &raquo;</span>
+              <span class="sr-only">Next</span>
+            </button>
+          </li>
+        </ul>
+      </nav>
+    </div>
   </div>
 </template>
 
 <script>
-import { mapState } from "vuex";
+import { mapState, mapActions, mapMutations } from "vuex";
 export default {
   computed: {
     ...mapState(["data", "pagination", "page"]),
-  },  
+    prevDisabled() {
+      return this.page == 1;
+    },
+    nextDisabled() {
+      return this.page == this.pagination.pages;
+    },
+  },
+  methods: {
+    ...mapActions(["getData"]),
+    ...mapMutations(["pageUp", "pageDown"]),
+    nextPage() {
+      if (this.page < this.pagination.pages) {
+        this.pageUp();
+        this.getData(`location?page=${this.page}`);
+      }
+    },
+    prevPage() {
+      if (this.page > 1) {
+        this.pageDown();
+        this.getData(`location?page=${this.page}`);
+      }
+    },
+  },
 };
 </script>
+
+},
 
 <style scoped></style>
